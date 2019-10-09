3eac280e9d3d01ab34541350e367d50c1cdacc35
santosh <a@b.com>
2019-10-09 18:28:05 +0530

    initial

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.quickdate.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | Quickdate - The Ultimate PHP Dating Platform
+// | Copyright (c) 2018 Quickdate. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$servername = "localhost";
+// MySQL Database User
+$dbusername = "root";
+// MySQL Database Password
+$dbpassword = "";
+// MySQL Database Name
+$dbname = "meetmyfetish";
+
+// Site URL
+$site_url = "http://localhost/whonearyou/"; // e.g (http://example.com)
+//$app = "QuickDating";
+$endpoint_v = "v1";
+//$serverkey = "9bd6376c02fc8627e94814064dc85f19";
+
+// Purchase code
+//$purchase_code = ""; // Your purchase code, don't give it to anyone.
+?>
